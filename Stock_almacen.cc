1ade037edcd5bc0095e48751e7557e6458a60953
Fernando Enrique Marroquín Hernandez <00007626@uca.edu.sv>
2026-09-08 16:00:47 -0600

    Clean up Stock_almacen.cc by removing unused code
    Removed unused function declarations and main function.

--- Stock_almacen.cc
+++ Stock_almacen.cc
@@ -1,20 +1,10 @@
 #include <iostream>
-
-int IngresarStock(int *stock, int cantidad);
-int RetirarStock(int *stock, int cantidad);
-int ConsultarStock(int stock);
-
-int main()
-{
-    int stock = 50;
     int cantidad = 34;
 
     RetirarStock(&stock, cantidad); 
 
     std::cout << "Se retiraron " << cantidad << " objetos del stock. Stock actual: " << stock <<"\n";
 
-    return 0;
-}
 
 int RetirarStock(int *stock, int cantidad)
 {
