234f451ffda96b59b7c9fb76b8064ac85afca40e
AlexanderMelara <00041126@uca.edu.sv>
2026-09-08 15:31:52 -0600

    "feat: ConsultarStock"

--- Stock_almacen.cc
+++ Stock_almacen.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 
-IngresarStock(int %stock, int cantidad);
-RetirarStock(int *stock, int cantida);
-ConsultarStock(int stock);
+int cantidad;
+int IngresarStock(int stock, int cantidad);
+int RetirarStock(int *stock, int cantida);
+void ConsultarStock(int *stock);
 
 int main(){
     
     return 0;
+}
+void ConsultarStock(int *stock){
+    std:: cout<<"El stock actual es: " <<cantidad;
 }
