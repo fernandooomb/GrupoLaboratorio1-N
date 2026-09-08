01b2507e28b5fa6580c2cb10e17f6d840e0dbb6f
GianfrancoMirandaHernandez <00059425@uca.edu.sv>
2026-09-08 15:30:38 -0600

    Repositorio1

--- Stock_almacen.cc
+++ Stock_almacen.cc
@@ -1,10 +1,32 @@
 #include <iostream>
 
-IngresarStock(int %stock, int cantidad);
-RetirarStock(int *stock, int cantida);
-ConsultarStock(int stock);
+struct Almacen
+{
+    int stock;
+};
+
+void IngresarStock(int &stock , int cantidad );
+void RetirarStock (int *stock, int cantidad);
+void ConsultarStock(int stock);
 
 int main(){
+    Almacen almacen;
+
+    almacen.stock = 50;
+
+    std::cout<<"Stock inicial:"<<almacen.stock<<"\n";
+
+    IngresarStock(almacen.stock, 20); 
+    std::cout<<"ingresando stock:"<<almacen.stock<<"\n"; 
+
+    
     
     return 0;
 }
+
+void IngresarStock(int &stock, int cantidad){
+
+        stock += cantidad ;
+        
+    }
+
