c79704aa315371b3e5366f55080a991312fd3245
fernandooomb <00007626@uca.edu.sv>
2026-09-08 15:47:34 -0600

    "feature: Funcion retirar stock"

--- Stock_almacen.cc
+++ Stock_almacen.cc
@@ -1,10 +1,23 @@
 #include <iostream>
 
-IngresarStock(int %stock, int cantidad);
-RetirarStock(int *stock, int cantida);
-ConsultarStock(int stock);
+int IngresarStock(int *stock, int cantidad);
+int RetirarStock(int *stock, int cantidad);
+int ConsultarStock(int stock);
 
-int main(){
-    
+int main()
+{
+    int stock = 50;
+    int cantidad = 34;
+
+    RetirarStock(&stock, cantidad); 
+
+    std::cout << "Se retiraron " << cantidad << " objetos del stock. Stock actual: " << stock <<"\n";
+
     return 0;
+}
+
+int RetirarStock(int *stock, int cantidad)
+{
+    *stock -= cantidad;
+    return *stock;
 }
